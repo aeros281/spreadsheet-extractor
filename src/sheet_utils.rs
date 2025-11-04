a41adf4c107f06864832264b7a9afcd79e84fdd1
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2025-11-04 20:24:31 +0700

    support path expand for Windows

--- src/sheet_utils.rs
+++ src/sheet_utils.rs
@@ -6,6 +6,9 @@ use std::path::PathBuf;
 use crate::config::{Config, Google};
 
 use google_sheets4::common;
+use google_sheets4::hyper_util::client::legacy::connect::HttpConnector;
+use hyper_rustls::HttpsConnector;
+use pathexpand::expand;
 use sheets4::Result;
 use sheets4::api::ValueRange;
 use sheets4::{Sheets, hyper_rustls, hyper_util, yup_oauth2};
@@ -15,6 +18,11 @@ pub async fn get_sheet_data(
     sheet_id: &str,
     range: &str,
 ) -> Result<(common::Response, ValueRange)> {
+    let hub = build_hub(&config).await?;
+    hub.spreadsheets().values_get(sheet_id, range).doit().await
+}
+
+pub async fn build_hub(config: &Config) -> Result<Sheets<HttpsConnector<HttpConnector>>> {
     let google_config: &Google = &config
         .google
         .as_ref()
@@ -22,6 +30,15 @@ pub async fn get_sheet_data(
 
     let service_account_path = build_path(&google_config.client_secret_path);
     let token_storage_path = build_path(&google_config.token_storage_path);
+
+    debug!(
+        "expanded service account path = {}",
+        service_account_path.display()
+    );
+    debug!(
+        "expanded token storage path = {}",
+        token_storage_path.display()
+    );
 
     let secret = yup_oauth2::read_application_secret(service_account_path).await
         .expect("Cannot find the application secret, please make sure to set the config for google.client_secret_path");
@@ -44,10 +61,9 @@ pub async fn get_sheet_data(
         );
 
     let hub = Sheets::new(client, auth);
-    hub.spreadsheets().values_get(sheet_id, range).doit().await
+    Ok(hub)
 }
 
 fn build_path(val: &str) -> PathBuf {
-    let expanded_path_str: std::borrow::Cow<str> = shellexpand::tilde(val);
-    PathBuf::from(expanded_path_str.as_ref())
+    expand(val).unwrap_or(PathBuf::from(val))
 }
